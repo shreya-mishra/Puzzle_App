f44c1ecdb33138c3bdd26b60cd8e8e066d9bbf40
shreya-mishra <shreyamishra040398@gmail.com>
2024-02-01 01:20:57 +0530

    added test case for rendering title

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {render, fireEvent, waitFor} from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import App from '../src/App';
-import renderer from 'react-test-renderer';
 
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+test('renders correctly', () => {
+  const {getByText} = render(<App />);
+  const linkElement = getByText(/Water Tank Simulation/i);
+  expect(linkElement).toBeTruthy();
 });
